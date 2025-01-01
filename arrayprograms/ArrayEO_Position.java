78f784c38b4ae8967154ae7fd2dfc4549252c7eb
DossTech <a.ranojidoss@gmail.com>
2025-01-01 13:13:13 +0530

    message

---
+++ arrayprograms/ArrayEO_Position.java
@@ -0,0 +1,26 @@
+public class ArrayEO_Position {
+    public static void main(String[] args) {
+        package org.programs;
+
+        public class ArrayEO_Position {
+        
+            public static void main(String[] args) {
+        
+                int a[] = {10,20,30,40,50};
+        
+                System.out.println("Array Even Position: ");
+        
+                for (int i = 0; i < a.length; i+=2) {
+                    System.out.println(a[i]);
+                }
+                
+                System.out.println("Array Odd Position: ");
+                
+                for (int i = 1; i < a.length; i+=2) {
+                    System.out.println(a[i]);
+                }
+            }
+        }
+        
+    }
+}
